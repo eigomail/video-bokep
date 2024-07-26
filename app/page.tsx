060e646e5d4104151a8cf5dcb5001e1feeebbbc6
eigomail <162259990+eigomail@users.noreply.github.com>
2024-07-27 04:06:45 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home({
     searchParams: { [key: string]: string | string[] | undefined };
 }) {
     const page =
-        (searchParams.page && parseInt(searchParams.page as string)) || 1;
+        (searchParams.page && parseInt(searchParams.page as string)) || 41;
     const per_page =
         (searchParams.per_page && parseInt(searchParams.per_page as string)) ||
         DEFAULT_PER_PAGE;
@@ -17,12 +17,13 @@ export default async function Home({
     const query = (searchParams.q && (searchParams.q as string)) || undefined;
 
     return (
-        <div className="md:my-2"><h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Barat Terbaru</h2>
+        <div className="md:my-2">
             {query ? (
                 <SearchCardList query={query} banner />
             ) : (
-                <SearchCardList query={SITENAME.split(" ")[1]} />
+                <CardList page={page} per_page={per_page} fld_id={fld_id} />
             )}
+<h1>{SITENAME}</h1><h2>Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru</h2><p>Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pijat pelajar Hijab Abg Colmek Film Tante Hot Twitter Asia Download Live stw situs bokep indonesia jepang barat korea japan jav cina japanese china rusia arab india thailand nonton link sd crot playbokep simontok bokepin montok baru perawan anak kecil telegram selingkuh ojol cantik gay vidio lokal artis pelajar janda streaming hd anime hentai bokepind gudang avtub pijat sotwe rumah pemerkosaan inggris xpanas pure tobrut vcs ngintip binor remaja yandex update perselingkuhan wiki raja full com porno indoh</p>
         </div>
     );
 }
