9c417167e28546d37c5ff6ce357b510a14dc0a03
eigomail <162259990+eigomail@users.noreply.github.com>
2025-05-08 23:53:22 +0700

    Update video-card.tsx

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -10,7 +10,7 @@ const VideoCard = ({ video }: any) => {
         <Card className="border-0 rounded-none md:border-[1px] md:rounded-md transform transition duration-200 md:hover:scale-[101%] md:hover:shadow-lg">
             <div className="relative">
                 <Link
-                    href={`/v/${video.file_code}#${video.title}`}
+                    href={`/v/${video.file_code}`}
                     title={`Watch ${video.title}`}
                     target="blank"
                     >
@@ -22,7 +22,7 @@ const VideoCard = ({ video }: any) => {
             </div>
             <CardContent className="p-1.5"><h3>
                 <Link
-                    href={`/v/${video.file_code}#${video.title}`}
+                    href={`/v/${video.file_code}`}
                     className="line-clamp-2 text-sm md:text-md font-semibold hover:text-primary focus:text-primary"
                     title={`Watch ${video.title}`}
                 >
